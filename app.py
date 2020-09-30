17e049ee12446b6fe6dc67e48341d676d1479e30
Mano <55449900+Mano181@users.noreply.github.com>
2020-09-30 11:10:20 +0530

    update app.py
    changed the datatype to float in requesting the form

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
     None.
 
     '''
-    int_features = [x for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction=model.predict(final_features)
     
